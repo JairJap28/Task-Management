39a35b9780d79cab1e51be7f2db3d1c6cb115e4f
Jair Jimenez Munevar <jair.jimenezm@perficient.com>
2021-12-13 21:41:16 -0500

    Create Get All tasks, Get Task by Id, Create Task, Update taks by Id, Delete Task and update task status method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './DTO/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,38 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTaskById(id: string) {
+    const indexTask = this.tasks.findIndex((task) => task.id === id);
+    if (indexTask !== -1) this.tasks.splice(indexTask, 1);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    if (task) {
+      task.status = status;
+    }
+
+    return task;
   }
 }
